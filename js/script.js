4515131086f64d7908315cb0a6e97481aafc9b8e
Jonatanehs <140452768+Jonatanehs@users.noreply.github.com>
2024-05-30 22:29:04 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-function show(){
-    document.querySelector('.hamburger').classList.toggle('open')
-    document.querySelector('.navigation').classList.toggle('active')
-}
+document.addEventListener("DOMContentLoaded", function() {
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+    console.log("funciona")
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+});
